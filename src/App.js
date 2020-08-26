72bb23908fc65d1567cf469ebd44a302e3ba56e1
Bruno-Nogueira <bruno1987tn@gmail.com>
2020-08-26 19:48:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      //
+    })
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const indice = repositories.findIndex(repository => repository.id === id)
+
+    const response = await api.delete('repositories', repositories[indice])
+
+    const repository = response.data
+
+    setRepositories(repositories.filter(e => e !== repository))
   }
 
   return (
